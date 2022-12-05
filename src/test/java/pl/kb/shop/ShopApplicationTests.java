2a9fe9b986d25844dce7e47b7bdc46e000e752c0
Kamil Biernat <k.biernat@yahoo.ca>
2022-12-05 13:35:26 +0100

    initial commit

---
+++ src/test/java/pl/kb/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kb.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
