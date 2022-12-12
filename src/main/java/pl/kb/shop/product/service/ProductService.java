41bce89546b84811e0ff523ee2f7b5ae8ccfdfdb
Kamil Biernat <k.biernat@yahoo.ca>
2022-12-12 10:54:50 +0100

    products pagination

--- src/main/java/pl/kb/shop/product/service/ProductService.java
+++ src/main/java/pl/kb/shop/product/service/ProductService.java
@@ -1,11 +1,12 @@
 package pl.kb.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import pl.kb.shop.product.model.Product;
 import pl.kb.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +14,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProduct() {
-        return productRepository.findAll();
+    public Page<Product> getProduct(@PageableDefault(size = 15) Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
