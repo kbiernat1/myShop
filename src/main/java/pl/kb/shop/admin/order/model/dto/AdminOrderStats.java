26ae8115cb9a7e47027d52ea9d19a7a40be23f48
Kamil Biernat <k.biernat@yahoo.ca>
2023-02-26 18:42:33 +0100

    sales chart

---
+++ src/main/java/pl/kb/shop/admin/order/model/dto/AdminOrderStats.java
@@ -0,0 +1,17 @@
+package pl.kb.shop.admin.order.model.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Builder
+public class AdminOrderStats {
+    private List<Integer> label;
+    private List<BigDecimal> sale;
+    private List<Long> order;
+    private Long ordersCount;
+    private BigDecimal salesSum;
+}
