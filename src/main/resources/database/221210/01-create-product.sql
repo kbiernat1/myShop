4486e68031c5d54a87fc64cb9ff5bef7be5d86e7
Kamil Biernat <k.biernat@yahoo.ca>
2023-04-14 11:46:34 +0200

    liqubase deployment error fix

--- src/main/resources/database/221210/01-create-product.sql
+++ src/main/resources/database/221210/01-create-product.sql
@@ -1,11 +1,12 @@
 --liquibase formatted sql
 --changeset kbiernat:1
+--validCheckSum: 8:cd2cfd8472be45e54973d8f3ad4b23f8
 create table product(
     id bigint not null auto_increment PRIMARY KEY,
     name varchar(255) not null,
     category varchar(255) not null,
     description text not null,
-    img varchar(255) not null,
+--     img varchar(255) not null,
     price decimal(9,2) not null,
     currency varchar(3) not null
 );
