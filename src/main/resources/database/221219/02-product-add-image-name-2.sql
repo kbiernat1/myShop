e7bce934972822a4958011639a2ff966c05f3653
Kamil Biernat <k.biernat@yahoo.ca>
2023-04-14 11:50:52 +0200

    liqubase deployment error fix

--- src/main/resources/database/221219/02-product-add-image-name-2.sql
+++ src/main/resources/database/221219/02-product-add-image-name-2.sql
@@ -1,3 +1,5 @@
 --liquibase formatted sql
 --changeset kbiernat:3
+--validCheckSum: 8:c26df93da243f3816107b3afa77aad87
+delete img from product;
 alter table product add img varchar(128) after currency;
